93d17dd534c9dcabfccd8d3d3d625b1dc85fa215
victorsootho <soothotsoenyane@gmail.com>
2024-06-02 16:12:08 +0200

    bufferline now shows the amount of error or warnings

--- lua/sootho/plugins/bufferline.lua
+++ lua/sootho/plugins/bufferline.lua
@@ -1,11 +1,25 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	version = "*",
+	opts = {
+		options = {
+			mode = "buffers",
+			separator_style = "slant",
+			hover = {
+				enabled = true,
+				delay = 200,
+				reveal = { "close" },
+			},
+			diagnostics = "nvim_lsp",
+			diagnostics_indicator = function(count, level, diagnostics_dict, context)
+				local s = " "
+				for e, n in pairs(diagnostics_dict) do
+					local sym = e == "error" and " " or (e == "warning" and " " or "")
+					s = s .. n .. sym
+				end
+				return s
+			end,
+		},
+	},
 }
