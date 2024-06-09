3a518f7473ae9017e6332566e77028c2ee59f9ef
victorsootho <soothotsoenyane@gmail.com>
2024-06-09 16:39:22 +0200

    indentation warnings, minor improvements

--- lua/sootho/core/keymaps.lua
+++ lua/sootho/core/keymaps.lua
@@ -24,10 +24,20 @@ keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) 
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
+-- tab management
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "<leader>tx", function()
+	if #vim.api.nvim_list_tabpages() > 1 then
+		vim.cmd("tabclose")
+	else
+		print("Cannot close the last tab page")
+	end
+end, { desc = "Close current tab" }) -- close current tab with a check
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) -- go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) -- go to previous tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) -- move current buffer to new tab
 
 -- Diagnostics
+keymap.set("n", "<C-j>", function()
+	vim.diagnostic.goto_next()
+end)
---
+++ lua/sootho/plugins/ui/indent-blankline.lua
@@ -0,0 +1,8 @@
+-- lua/sootho/plugins/ui/indent-blankline.lua
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	event = "LazyFile",
+	opts = {
+		scope = { enabled = false },
+	},
+}
